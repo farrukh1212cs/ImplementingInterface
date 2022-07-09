b5b811955d49c84111ba9ad93de74df5f43d4741
Farrukh Rehman <farrukh1212cs@outlook.com>
2022-07-09 05:10:25 +0500

    cin

---
+++ ImplementingInterface/Repo/TConverter.cs
@@ -0,0 +1,13 @@
+﻿using ImplementingInterface.Interface;
+using ImplementingInterface.Models;
+using ImplementingInterface.Repo.IRepo;
+
+namespace ImplementingInterface.Repo
+{
+    public class TConverter : TableReader<Root>, ITConverter
+    {
+        public TConverter(IDataConverter<Root> dataConverter) : base(dataConverter)
+        {
+        }
+    }
+}
